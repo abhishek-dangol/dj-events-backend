f561e7b5eaf050efe0c2b38103dfdbd15a3c9272
abhishek-dangol <abhi.dangol@gmail.com>
2021-08-04 23:20:02 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3296e56437ded3759f4ab3c73de8201b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1e22de96-cbde-4282-b8c1-4cde18697758'
+};
